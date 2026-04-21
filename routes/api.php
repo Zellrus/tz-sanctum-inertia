2db21918dc0381ae39b0804db98b7f72072840e9
zellrus <aleksei.shvec96@gmail.com>
2026-04-21 21:48:45 +0300

    add auth api methods

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,20 @@
 <?php
 
+use App\Http\Controllers\api\auth\AuthController;
 use App\Http\Controllers\api\CategoryController;
 use App\Http\Controllers\api\ProductController;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
 //Route::prefix('v1')->group(function () {
 //
 //}
 
-Route::apiResource('products', ProductController::class)
-    ->only(['index', 'show']);
+Route::apiResource('products', ProductController::class)->only(['index', 'show']);
 Route::get('categories', [CategoryController::class, 'index']);
-
+Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('products', ProductController::class)
-        ->except(['index', 'show']);
+    Route::get('/user', function (Request $request) {return $request->user();});
+    Route::apiResource('products', ProductController::class)->except(['index', 'show']);
+    Route::post('logout', [AuthController::class, 'logout']);
 });
